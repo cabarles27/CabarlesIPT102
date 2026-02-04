bf8cd0b1db44b80cfaf071611ad4adda768d1fe6
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2026-02-04 08:09:35 +0800

    Add files via upload

---
+++ CabarlesIPT102/CabarlesFramework/Comands/DeletePOS.cs
@@ -0,0 +1,36 @@
+﻿using CabarlesDomain.Commands;
+using CabarlesFramework.Comands; 
+using CabarlesFramework.Extensions;
+using CabarlesFramework.Models;
+using CabarlesRepository.Interface;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CabarlesFramework.Coomands
+{
+    
+    public class DeletePOS : IDeletePOS
+    {
+        private readonly string _connectionName = "DefaultConnection";
+        private readonly string _storedProcedureName;
+        private readonly IRepository _reposository;
+
+        public DeletePOS(IRepository reposository)
+        {
+            _storedProcedureName = "DeletePOS";
+            _reposository = reposository;
+        }
+
+        public async Task<bool> ExecuteAsync(POS_Models model)
+        {
+            var param = model.ToPOSDynamicParameters();
+            return await _reposository.SaveDataAsync(
+                _connectionName,
+                _storedProcedureName,
+                param
+            );
+        }
+    }
+}
+
---
+++ CabarlesIPT102/CabarlesFramework/Queries/GetPOS.cs
@@ -0,0 +1,39 @@
+﻿using CabarlesRepository.Interface;
+using CabarlesFramework.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabarlesFramework.Queries
+{
+    public interface IGetPOSById
+    {
+        Task<POS_Models?> ExecuteAsync(int posId);
+    }
+    public class GetEmployeeById : IGetPOSById
+    {
+        private readonly string _connectionName = "DefaultConnection";
+        private readonly string _storeProcedureName;
+        private readonly IRepository _repository;
+
+        public GetEmployeeById(IRepository repository)
+        {
+            _storeProcedureName = "GetPOS";
+            _repository = repository;
+        }
+
+        public async Task<POS_Models?> ExecuteAsync(int Id)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("Id", Id);
+            var data = await _repository.GetDataAsync<POS_Models>(_connectionName, _storeProcedureName, parameters);
+            return data.FirstOrDefault();
+        }
+    }
+}
+    
+
